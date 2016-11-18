9dbef64a35fd2ccaf89041abc4d05709d4b351fa
pachecohjr <hjpachecker@gmail.com>
2016-11-18 08:56:23 -0500

    Update fig06_30.cpp

--- fig06_30.cpp
+++ fig06_30.cpp
@@ -1,2 +1,6 @@
 unsigned long fibonacci (unsigned long number)
-{
+{ if ((number == 0) || (number == 1)) // base cases
+  return number; 
+ else  // recursion step
+   return (fibonacci(number - 1) + fibonacci(number - 2)); 
+} // end function fibonacci
