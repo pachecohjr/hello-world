e3fc4858da85e83209d1c3057926a54baf8ee76f
pachecohjr <hjpachecker@gmail.com>
2016-11-18 08:51:08 -0500

    Update fig06_29.cpp

--- fig06_29.cpp
+++ fig06_29.cpp
@@ -1,4 +1,8 @@
+// recursive definition of function factorial
 unsigned long factorial (unsigned long number)
 {
-
-}
+  if (number <= 1) // test for base case
+    return number; // base cases: 0! = 1  and 1! = 1
+  else // recursion step
+    return number * factorial(number - 1); 
+} // end function factorial
