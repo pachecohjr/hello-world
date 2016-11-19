0a99a21eea6593a53ae097609107efa99282235c
pachecohjr <hjpachecker@gmail.com>
2016-11-19 09:28:16 -0500

    Update exer06_29.cpp

--- exer06_29.cpp
+++ exer06_29.cpp
@@ -4,5 +4,19 @@ int reverse (int);
 
 int main (void)
 {
+  int n;
+  
+  std::cout << "Enter an integer, it will show in reverse: ";
+  std::cin  >> n;
+  
+  std::cout << "You entered " << n << "\n";
+  std::cout << "In reverse: " << reverse(n) << std::endl;
+}
 
+int reverse (int number)
+{
+ int rem = number;
+  while (rem > 0) {
+  std::cout << rem %= 10 <<;
+  rem /= 10; }
 }
