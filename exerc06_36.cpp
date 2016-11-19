d0432aa49eabe99a32de5a258f601764a1d6a4d9
pachecohjr <hjpachecker@gmail.com>
2016-11-19 08:25:58 -0500

    Update exerc06_36.cpp

--- exerc06_36.cpp
+++ exerc06_36.cpp
@@ -5,5 +5,20 @@ int power (int, int);
 
 int main (void)
 {
+  int base, exp;
+  
+  std::cout << "Enter an integer followed by another integer for it's exponent: \n\n";
+  std::cin >> base >> exp;
+  
+  std::cout << base << " in the power of " << exponent << " is " << power(base, exp) << std::endl;
+  
+  return 0;
+}
 
+int power (int a, int x);
+{
+ if (x == 1)
+   return a;
+ else 
+   return (a * power(a, x-1);  
 }
