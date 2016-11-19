0069c05ba83dcd2ffe0555685b44d8163d857395
pachecohjr <hjpachecker@gmail.com>
2016-11-19 13:12:39 -0500

    Update exer06_31.cpp

--- exer06_31.cpp
+++ exer06_31.cpp
@@ -4,5 +4,25 @@ int gcd (int, int);
 
 int main (void)
 {
+  int a, b;
+  
+  std::cout << "Enter first number: ";
+  std::cin  >> a;
+  std::cout << "Enter second number: ";
+  std::cin  >> b;
+  
+  std::cout << "The G.C.D. of " << a << " and " << b << " is " << gcd(a,b) << std::endl;
+  
+  return 0;
+}
 
+int gcd (int a, int b) // local a and and local b
+{
+  // if b divides 
+ if (b % a == 0) 
+   return a;
+  if (a > b)
+    return gcd (b, a);
+  else
+    return gcd (b % a, a);
 }
