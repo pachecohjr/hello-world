9a351522e88df004c26fa55b519e6ad49e58e1df
pachecohjr <hjpachecker@gmail.com>
2016-11-27 08:54:19 -0500

    Create fig17_15.cpp

---
+++ fig17_15.cpp
@@ -0,0 +1,5 @@
+// Fig 17.15: fig17_15.cpp
+// This program reads a random-access file sequentially, updates
+// data previously written to the file, creates data to be placed
+// in the file, and deletes data previously stored in the file.
+
