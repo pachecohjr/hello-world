f611b99bad6bdc93f6d59159989f5a2a9e5ea2e0
pachecohjr <hjpachecker@gmail.com>
2016-11-27 14:39:40 -0500

    Update fig18_01.cpp

--- fig18_01.cpp
+++ fig18_01.cpp
@@ -6,5 +6,23 @@
 
 int main (void)
 {
-
+  string string1("cat");
+  string string2; // initialized to the empty string
+  string string3; // initialized to the empty string
+  
+  string2 = string1;
+  string3.assign(string1);
+  std::cout << "string1: " << string1 << "\nstring2: " << string2
+    << "\nstring3: " << string3 << "\n\n";
+  
+  // modify string2 and string3
+  string2[0] = string3[2] = 'r';
+  
+  std::cout << "After modification of string2 and string3:\n" << "string1: "
+    << string1 << "\nstring2: " << string2 << "\nstring3: ";
+  
+  // demonstrating member function at
+  for (int i = 0; i < string3.length(); ++i)
+    std::cout << string3.at(i);
+  
 }
