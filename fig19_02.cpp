f7c8a2c7949e69afa9f4f23b5ed67499fcd61f9b
pachecohjr <hjpachecker@gmail.com>
2016-11-29 18:50:12 -0500

    Update fig19_02.cpp

--- fig19_02.cpp
+++ fig19_02.cpp
@@ -3,3 +3,15 @@
 // that uses binary search to find an integer.
 #include <vector>
 
+class BinarySearch
+{
+  public:
+  BinarySearch (int ); // constructor initializes vector
+  int binarySearch (int ) const; // perform a binary search on vector
+  void displayElements() const; // display vector elements
+  private:
+  int size_; // vector size
+  vector<int> data; // vector of ints
+  void displaySubElements (int, int) const; // display range of values
+}; // end class BinarySearch
+  
