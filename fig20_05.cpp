500bfc43e2816d8fb32674a4a5e4206518c0deed
pachecohjr <hjpachecker@gmail.com>
2016-12-03 12:45:05 -0500

    Update fig20_05.cpp

--- fig20_05.cpp
+++ fig20_05.cpp
@@ -1,2 +1,28 @@
 // Fig. 20.4: list.h
 // Template List class definition
+#ifndef _LIST_H
+#define _LIST_H
+
+#include <iostream>
+#include <listnode.h>  // ListNode class definition
+
+template<typename NODETYPE>
+class List
+{
+  public:
+  List(); // constructor
+  ~List();  // destructor
+  void insertAtFront(const NODETYPE & ); 
+  void insertAtBack (const NODETYPE & );
+  bool removeFromFront (NODETYPE & );
+  bool removeFromBack (NODETYPE & );
+  bool isEmpty() const;
+  bool print() const;
+  
+  private:
+  ListNode<NODETYPE> *firstPtr_; // pointer to first node
+  ListNode<NODETYPE> *lastPtr_;  // pointer to last node  
+
+// utility function to allocate new node
+ListNode <NODETYPE> *getNewNode (const NODETYPE & );
+}; // end class List
