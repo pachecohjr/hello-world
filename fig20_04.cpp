fd296308d48aa1028af151a19bf1be3adbdc03a9
pachecohjr <hjpachecker@gmail.com>
2016-12-03 12:29:19 -0500

    Update fig20_04.cpp

--- fig20_04.cpp
+++ fig20_04.cpp
@@ -1,2 +1,21 @@
-// Fig. 20.4: List.h
-// Template List class definition
+// Fig 20.3: listnode.h
+// Template ListNode class definition
+#ifndef _LISTNODE_H
+#define _LISTNODE_H
+
+// forward declaration of class List required to announce that class
+// List exists so it can be used in the friend declaration at line 13
+template<typename NODETYPE> class List;
+
+template<typename NODETYPE>
+class ListNode
+{
+  friend class List<NODETYPE>; // make List a friend
+  
+  public:
+  ListNode (const NODETYPE &); // constructor
+  NODETYPE getData() const; // return data in node
+  private:
+  NODETYPE data; // data
+  ListNode<NODETYPE> *nextPtr; // next node in list  
+}; // end class ListNode
