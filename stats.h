dfe6c6e819aca5801f026edcf532978fceac8515
pachecohjr <hjpachecker@gmail.com>
2016-12-20 08:25:14 -0500

    Create stats.h

---
+++ stats.h
@@ -0,0 +1,6 @@
+#include <iomanip>
+
+float Mean   (const int* array, size_t size); // calculates mean of data in array
+float Median (int* array, size_t size);       // calculates median of data in array
+void  Swap   (int& x, int& y);                // interchanges values of x and y
+void  Sort   (int* array, size_t size);       // sorts the data in array
