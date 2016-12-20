707b4392b0895779069be3bdd7db5d68aa6171a0
pachecohjr <hjpachecker@gmail.com>
2016-12-20 14:57:23 -0500

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -7,5 +7,38 @@ void  Sort   (int* array, size_t size);       // sorts the data in array
 
 float Mean   (const int* array, size_t size)
 {
+  int sum = 0;
+  
+  for (size_t i = 0; i < size; ++i)
+      sum += array[i];
+  
+  return sum / size;  
 }
 
+float Median (int* array, size_t size)
+{
+  Sort (array, size);
+  
+  for (size_t i = 0; i < size; ++i)
+  {
+    if (size % 2 != 0)
+      return array[(size - 1) / 2];
+    else
+      return array[size / 2 - 1] + array[size / 2];
+  }
+}
+
+void  Sort   (int* array, size_t size)
+{
+  for (int i = 0; i < size - 1; ++i)
+  {
+    int smallest = array[0];
+  
+  for (size_t j = 1; i < size - 1; ++i)
+  {
+    if (array[i] > array[i + 1])
+      Swap(a[i], a[i + 1]);
+  }
+  }
+  
+}
