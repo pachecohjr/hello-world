f8b350a3481dac243c9205f5453e624bd4b338e2
pachecohjr <hjpachecker@gmail.com>
2016-12-20 08:26:06 -0500

    Create stats.cpp

---
+++ stats.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+float Mean   (const int* array, size_t size); // calculates mean of data in array
+float Median (int* array, size_t size);       // calculates median of data in array
+void  Swap   (int& x, int& y);                // interchanges values of x and y
+void  Sort   (int* array, size_t size);       // sorts the data in array
+
+float Mean   (const int* array, size_t size)
+{
+}
+
